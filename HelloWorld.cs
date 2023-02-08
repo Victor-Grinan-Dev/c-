6b2ec6d8aaa5d387826e223d72c294217ccfc887
Victor <victorgrinan@gmail.com>
2023-02-08 13:14:29 +0200

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            console.ReadLine();
+        }
+    }
+}
---
+++ Varibles.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Giraffe
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+            Console.WriteLine("there was a man named " + characterName);
+            Console.WriteLine("He was 35 years old");
+            Console.WriteLine("He really liked the name John");
+            Console.WriteLine("But he didn't liked being 35");
+
+            console.ReadLine();
+        }
+    }
+}
