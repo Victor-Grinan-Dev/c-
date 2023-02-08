3fff20f8fa477fd4fb0e5ca4144509e89fce4c2c
Victor <victorgrinan@gmail.com>
2023-02-08 14:04:58 +0200

    Calculator

---
+++ StrToNum.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Giraffe
+{
+    class StrToNum
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            Console.ReadLine();
+        }
+    }
+}
