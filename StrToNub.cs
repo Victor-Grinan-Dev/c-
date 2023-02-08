cea7327319242b353f5907ce70dbba49ede7ea22
Victor <victorgrinan@gmail.com>
2023-02-08 14:00:24 +0200

    convert

---
+++ StrToNub.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Giraffe
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            Console.ReadLine();
+        }
+    }
+}
