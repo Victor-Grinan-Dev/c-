d7dd02715d6cf718358d50e7c84d65cd79b69180
Victor <victorgrinan@gmail.com>
2023-02-08 13:50:14 +0200

    update

---
+++ Numbers.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Giraffe
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 + 3);
+            Console.WriteLine(2 - 3);
+            Console.WriteLine(2 * 3);
+            Console.WriteLine(6 / 3);
+            Console.WriteLine(2 / 3);
+            Console.WriteLine(2 / 3.0);
+            Console.WriteLine(2 % 3);
+            Console.WriteLine(2 + 3 * 2);
+            Console.WriteLine((2 + 3)* 2);
+
+            console.ReadLine();
+        }
+    }
+}
