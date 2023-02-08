11860a520887bb67ca4e087e19c90c93e243109e
Victor <victorgrinan@gmail.com>
2023-02-08 13:21:32 +0200

    updated

--- Varibles.cs
+++ Varibles.cs
@@ -9,10 +9,19 @@ namespace Giraffe
             int characterAge;
             characterAge = 35;
             Console.WriteLine("there was a man named " + characterName);
-            Console.WriteLine("He was 35 years old");
+            Console.WriteLine("He was " + characterAge + " years old");
             Console.WriteLine("He really liked the name John");
-            Console.WriteLine("But he didn't liked being 35");
+            Console.WriteLine("But he didn't liked being " + characterAge);
 
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.3;
+            float lesstAcurate = 3.3;
+            decimal mostAcurate = 3.3;
+            bool isMale = true;
+
+            float, decimal = 
             console.ReadLine();
         }
     }
