0a960b980a4808ef650f916dd463728c173a675d
Victor <victorgrinan@gmail.com>
2023-02-09 14:58:04 +0200

    updated

---
+++ IfStatemenComparison.cs
@@ -0,0 +1,33 @@
+using System;
+namespace Giraffe
+{
+    class IfStatemenComparison
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+
+            Console.WriteLine(GetMax(num1, num2, num3));
+
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            int result;
+            if(num1 > num2 && num1 > num3)
+            {
+                result = num1;
+            } 
+            else if ( num2 > num1 && num2 > num3 ){
+                result = num2;
+            }
+            else {
+                result = num3;
+            }
+            return result;
+        }
+    }
+}
