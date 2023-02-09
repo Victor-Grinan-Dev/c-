d951ed8a5f4cfe7ca6f7321d9b2242e3192acdcd
Victor <victorgrinan@gmail.com>
2023-02-09 15:47:09 +0200

    updated

---
+++ ClassesAndObjects.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Giraffe
+{
+    class ClassesAndObjects
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
